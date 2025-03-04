a51a1b72367e198d0045eb4670afacee94e56a0f
adnanmusil <adnan.museel123@gmail.com>
2025-03-04 16:01:00 +0500

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ function App() {
   return (
    <main className="relative min-h-screen w-screen overflow-x-hidden">
     <Hero />
+
+    <section className="z-0 min-h-screen bg-blue-300" />
+
    </main>
   );
 }
