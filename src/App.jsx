c53b5e4af12c9749c3526c0c292a09aa4afc64ad
adnanmusil <adnan.museel123@gmail.com>
2025-03-08 17:03:35 +0500

    about section work and animated text work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
 function App() {
   return (
    <main className="relative min-h-screen w-screen overflow-x-hidden">
     <Hero />
-
-    <section className="z-0 min-h-screen bg-blue-300" />
-
+    <About />
    </main>
   );
 }
