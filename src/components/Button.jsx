51777f184dcec0690797faa20328e627cb546efc
adnanmusil <adnan.museel123@gmail.com>
2025-04-05 22:31:23 +0500

    updated work

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({title,id,containerClass,leftIcon,rightIcon}) {
 
   return (
-    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 hover:bg-[#5542ff] hover:text-white px-7 py-3 text-black ${containerClass}`}>
         {leftIcon}
         <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
             <div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -139,7 +139,7 @@ const Hero = () => {
                     <div className='mt-24 px-5 sm:px-10'>
                         <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
                         <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>Enter the Metagame Layer <br /> Unleash the Play Economy </p>
-                        <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
+                        <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 hover:!bg-yellow-400 hover:text-black flex-center gap-1" />
                     </div>
                 </div>
             </div>
