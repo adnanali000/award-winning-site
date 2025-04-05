6dc413d622d6bc23d262b343551bf623b8ab1503
adnanmusil <adnan.museel123@gmail.com>
2025-04-05 16:29:00 +0500

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
   return (
    <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -11,6 +13,8 @@ function App() {
     <About />
     <Features />
     <Story />
+    <Contact />
+    <Footer />
    </main>
   );
 }
